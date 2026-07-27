237dabf8950ea1386aed8df7046b0443b5e8f85c
BaselAlarashi <www.basel10001@gmail.com>
2026-07-27 20:27:23 +0300

    Carry 'RowVersion'  for topic update.

---
+++ backend/TreeNote.Application/Commands/Update/UpdateTopicPositionsCommand.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TreeNote.Application.Commands;
+
+public record TopicPositionUpdate(Guid Id, double X, double Y, byte[] RowVersion);
+
+public record UpdateTopicPositionsCommand([Required, MinLength(1)] List<TopicPositionUpdate> Positions);
